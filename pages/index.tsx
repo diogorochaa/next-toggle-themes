5b2cfdcee4add896f76f9946b232c037cdb1a8eb
DiogoRocha10 <diogorocha12@live.com>
2022-07-11 10:55:50 -0300

    feat: ajustando erro do server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+  if (!mounted) {
+    return null;
+  }
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl">Hello</h1>
@@ -9,7 +15,7 @@ export default function Home() {
         className="px-6 py-2 mt-10 bg-black dark:bg-white text-white dark:text-black "
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       >
-        Toggle to {theme === "ligth" ? "Dark" : "Ligth"}
+        Toggle to {theme === "light" ? "Dark" : "Light"}
       </button>
     </div>
   );
